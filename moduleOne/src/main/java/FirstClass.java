1d522c424da9f86177ca399911995a19c4c56e93
Yingrui <test@mail.com>
2020-05-08 14:47:38 +0800

    添加了 789

--- moduleOne/src/main/java/FirstClass.java
+++ moduleOne/src/main/java/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("456");
+        System.out.println("789");
     }
 
 }
